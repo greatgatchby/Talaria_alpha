8d30739e4d49a135ef03afe17d9bce99597415c0
Cassidy <cassidyrjones0@gmail.com>
2021-10-29 16:55:36 +0100

    Change 'user' to 'inventory', apply link to vendor page Get rid of authentication in menu Get rid of footer

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -89,123 +89,94 @@ const Dashboard = () => {
               </CButtonGroup>
             </CCol>
           </CRow>
-          <CChartLine
-            style={{ height: '300px', marginTop: '40px' }}
-            data={{
-              labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-              datasets: [
-                {
-                  label: 'My First dataset',
-                  backgroundColor: hexToRgba(getStyle('--cui-info'), 10),
-                  borderColor: getStyle('--cui-info'),
-                  pointHoverBackgroundColor: getStyle('--cui-info'),
-                  borderWidth: 2,
-                  data: [
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                  ],
-                  fill: true,
-                },
-                {
-                  label: 'My Second dataset',
-                  backgroundColor: 'transparent',
-                  borderColor: getStyle('--cui-success'),
-                  pointHoverBackgroundColor: getStyle('--cui-success'),
-                  borderWidth: 2,
-                  data: [
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                  ],
-                },
-                {
-                  label: 'My Third dataset',
-                  backgroundColor: 'transparent',
-                  borderColor: getStyle('--cui-danger'),
-                  pointHoverBackgroundColor: getStyle('--cui-danger'),
-                  borderWidth: 1,
-                  borderDash: [8, 5],
-                  data: [65, 65, 65, 65, 65, 65, 65],
-                },
-              ],
-            }}
-            options={{
-              maintainAspectRatio: false,
-              plugins: {
-                legend: {
-                  display: false,
-                },
-              },
-              scales: {
-                x: {
-                  grid: {
-                    drawOnChartArea: false,
-                  },
-                },
-                y: {
-                  ticks: {
-                    beginAtZero: true,
-                    maxTicksLimit: 5,
-                    stepSize: Math.ceil(250 / 5),
-                    max: 250,
-                  },
-                },
-              },
-              elements: {
-                line: {
-                  tension: 0.4,
-                },
-                point: {
-                  radius: 0,
-                  hitRadius: 10,
-                  hoverRadius: 4,
-                  hoverBorderWidth: 3,
-                },
-              },
-            }}
-          />
-        </CCardBody>
-        <CCardFooter>
-          <CRow xs={{ cols: 1 }} md={{ cols: 5 }} className="text-center">
-            <CCol className="mb-sm-2 mb-0">
-              <div className="text-medium-emphasis">Visits</div>
-              <strong>29.703 Users (40%)</strong>
-              <CProgress thin className="mt-2" precision={1} color="success" value={40} />
-            </CCol>
-            <CCol className="mb-sm-2 mb-0">
-              <div className="text-medium-emphasis">Unique</div>
-              <strong>24.093 Users (20%)</strong>
-              <CProgress thin className="mt-2" precision={1} color="info" value={40} />
-            </CCol>
-            <CCol className="mb-sm-2 mb-0">
-              <div className="text-medium-emphasis">Pageviews</div>
-              <strong>78.706 Views (60%)</strong>
-              <CProgress thin className="mt-2" precision={1} color="warning" value={40} />
-            </CCol>
-            <CCol className="mb-sm-2 mb-0">
-              <div className="text-medium-emphasis">New Users</div>
-              <strong>22.123 Users (80%)</strong>
-              <CProgress thin className="mt-2" precision={1} color="danger" value={40} />
-            </CCol>
-            <CCol className="mb-sm-2 mb-0">
-              <div className="text-medium-emphasis">Bounce Rate</div>
-              <strong>Average Rate (40.15%)</strong>
-              <CProgress thin className="mt-2" precision={1} value={40} />
-            </CCol>
-          </CRow>
-        </CCardFooter>
-      </CCard>
+      {/*    <CChartLine*/}
+      {/*      style={{ height: '300px', marginTop: '40px' }}*/}
+      {/*      data={{*/}
+      {/*        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],*/}
+      {/*        datasets: [*/}
+      {/*          {*/}
+      {/*            label: 'My First dataset',*/}
+      {/*            backgroundColor: hexToRgba(getStyle('--cui-info'), 10),*/}
+      {/*            borderColor: getStyle('--cui-info'),*/}
+      {/*            pointHoverBackgroundColor: getStyle('--cui-info'),*/}
+      {/*            borderWidth: 2,*/}
+      {/*            data: [*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*            ],*/}
+      {/*            fill: true,*/}
+      {/*          },*/}
+      {/*          {*/}
+      {/*            label: 'My Second dataset',*/}
+      {/*            backgroundColor: 'transparent',*/}
+      {/*            borderColor: getStyle('--cui-success'),*/}
+      {/*            pointHoverBackgroundColor: getStyle('--cui-success'),*/}
+      {/*            borderWidth: 2,*/}
+      {/*            data: [*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*              random(50, 200),*/}
+      {/*            ],*/}
+      {/*          },*/}
+      {/*          {*/}
+      {/*            label: 'My Third dataset',*/}
+      {/*            backgroundColor: 'transparent',*/}
+      {/*            borderColor: getStyle('--cui-danger'),*/}
+      {/*            pointHoverBackgroundColor: getStyle('--cui-danger'),*/}
+      {/*            borderWidth: 1,*/}
+      {/*            borderDash: [8, 5],*/}
+      {/*            data: [65, 65, 65, 65, 65, 65, 65],*/}
+      {/*          },*/}
+      {/*        ],*/}
+      {/*      }}*/}
+      {/*      options={{*/}
+      {/*        maintainAspectRatio: false,*/}
+      {/*        plugins: {*/}
+      {/*          legend: {*/}
+      {/*            display: false,*/}
+      {/*          },*/}
+      {/*        },*/}
+      {/*        scales: {*/}
+      {/*          x: {*/}
+      {/*            grid: {*/}
+      {/*              drawOnChartArea: false,*/}
+      {/*            },*/}
+      {/*          },*/}
+      {/*          y: {*/}
+      {/*            ticks: {*/}
+      {/*              beginAtZero: true,*/}
+      {/*              maxTicksLimit: 5,*/}
+      {/*              stepSize: Math.ceil(250 / 5),*/}
+      {/*              max: 250,*/}
+      {/*            },*/}
+      {/*          },*/}
+      {/*        },*/}
+      {/*        elements: {*/}
+      {/*          line: {*/}
+      {/*            tension: 0.4,*/}
+      {/*          },*/}
+      {/*          point: {*/}
+      {/*            radius: 0,*/}
+      {/*            hitRadius: 10,*/}
+      {/*            hoverRadius: 4,*/}
+      {/*            hoverBorderWidth: 3,*/}
+      {/*          },*/}
+      {/*        },*/}
+      {/*      }}*/}
+      {/*    />*/}
+      {/*  </CCardBody>*/}
+      {/*</CCard>*/}
 
-      <WidgetsBrand withCharts />
+      {/*<WidgetsBrand withCharts />*/}
 
       <CRow>
         <CCol xs>
