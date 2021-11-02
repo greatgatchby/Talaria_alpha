d85f562e79030c9b31caa1d2eca643747cf352d3
greatgatchby <40676529+greatgatchby@users.noreply.github.com>
2021-11-02 12:44:39 +0000

    Snekaer search results

--- src/views/Merchant/SneakerSearch/SneakerSearch.js
+++ src/views/Merchant/SneakerSearch/SneakerSearch.js
@@ -1,5 +1,19 @@
 import React from 'react'
-import { CFormInput, CFormLabel, CInputGroup, CRow } from '@coreui/react/dist'
+import {
+  CFormInput,
+  CFormLabel,
+  CInputGroup,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableFoot,
+  CTableHead,
+  CTableHeaderCell,
+  CContainer,
+  CCard,
+  CTableRow,
+} from '@coreui/react/dist'
 
 const SneakerSearch = () => {
   return (
@@ -14,6 +28,38 @@ const SneakerSearch = () => {
             <CFormInput />
           </CFormLabel>
         </CInputGroup>
+      </CRow>
+      <CRow className={'d-flex justify-content-center'}>
+        <CContainer>
+          <CCard>
+            <CTable hover responsive align="middle" className="mb-0 border text-center">
+              <CTableHead color="light">
+                <CTableRow>
+                  <CTableHeaderCell></CTableHeaderCell>
+                  <CTableHeaderCell>Name</CTableHeaderCell>
+                  <CTableHeaderCell>Stylecode</CTableHeaderCell>
+                  <CTableHeaderCell>Brand</CTableHeaderCell>
+                  <CTableHeaderCell>Average price</CTableHeaderCell>
+                </CTableRow>
+              </CTableHead>
+              <CTableBody>
+                <CTableDataCell>
+                  <img
+                    width={'100px'}
+                    src={
+                      'https://cms-cdn.thesolesupplier.co.uk/2020/12/jordan-1-high-og-pollen-1_w900.jpg.webp'
+                    }
+                  />
+                </CTableDataCell>
+                <CTableDataCell>Jordan 1 Pollen</CTableDataCell>
+                <CTableDataCell>555088-701</CTableDataCell>
+                <CTableDataCell>Jordan Brand</CTableDataCell>
+                <CTableDataCell>£170</CTableDataCell>
+              </CTableBody>
+              <CTableFoot></CTableFoot>
+            </CTable>
+          </CCard>
+        </CContainer>
       </CRow>
     </>
   )
